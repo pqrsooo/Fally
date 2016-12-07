b33a93d31520f654a423ac567d9276fb92a74ffc
Parinthorn Saithong <parinthorn.sa@gmail.com>
2016-12-07 22:07:08 +0700

    Implement timer and UI

--- Fally WatchKit Extension/InterfaceController.swift
+++ Fally WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,18 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet var counterLabel: WKInterfaceLabel!
+    
+    var timerCounter = 30
+    var timer = Timer()
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        setTitle("Fally")
+        initTimer()
     }
     
     override func willActivate() {
@@ -26,6 +33,23 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func initTimer() {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true);
+        
+        // Set init counter label
+        let timeString = String(format: "00:%02d", timerCounter)
+        counterLabel.setText("\(timeString)")
+    }
+    
+    func updateTimer() {
+        timerCounter -= 1
+        
+        // Update counter label
+        let timeString = String(format: "00:%02d", timerCounter)
+        counterLabel.setText("\(timeString)")
     }
 
 }
