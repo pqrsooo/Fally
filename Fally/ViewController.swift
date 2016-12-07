9d45649c19928c3358e0aac31d0f06d727315505
Parinthorn Saithong <parinthorn.sa@gmail.com>
2016-12-07 18:20:30 +0700

    Initial Commit

---
+++ Fally/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fally
+//
+//  Created by Parinthorn Saithong on 12/7/2016.
+//  Copyright © 2016 Parinthorn Saithong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
